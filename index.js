9b34532e480a6ac015f61d887e5d20e0d4e32ba8
Alaa zaza <alaa.kh.zaza@gmail.com>
2023-04-02 22:40:58 +0200

    Create routes folder with auth file and register route

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const app = express(); 
 
+// Impport Routes
+const authRoute = require('./routes/auth');
+
+// Route Middlewares
+app.use('/api/user/', authRoute)
+
+
 app.listen(3000, () => {
     console.log("The Server up and running!")
 })
