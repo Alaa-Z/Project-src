ba9d3777d82c015ba01fe5573ac1d4a2172155d1
Alaa zaza <alaa.kh.zaza@gmail.com>
2023-04-04 22:39:02 +0200

    install @hapi/joi to validate the whene user register

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,18 @@
 const router = require('express').Router();
 const User = require('../model/User')
 
+// import validate
+const {registerValidation} = require('../validation')
+
 
 router.post('/register', async (req, res) => {
+
+    // Validate the data 
+    const {error} = registerValidation(req.body)
+    //   If there is an error 
+    if(error) {
+        return res.status(400).send(error.details[0].message)
+    }
     const user = new User({
         name : req.body.name,
         email : req.body.email,
@@ -10,7 +20,7 @@ router.post('/register', async (req, res) => {
     });
     try {
         const savedUser = await user.save();
-    } catch(error){
+    } catch (error) {
         res.status(400).send(error)
     }
 })
---
+++ validation.js
@@ -0,0 +1,16 @@
+
+// VALIDATION 
+const Joi = require('@hapi/joi');
+
+// VALIDATE REGISTER 
+const registerValidation =  (data) => {
+    const schema = Joi.object(
+        {
+            name: Joi.string().min(2).max(255).required(),
+            email: Joi.string().min(6).required().max(255).email(),
+            password: Joi.string().min(6).max(1024).required() 
+        }
+    );
+    return schema.validate(data)
+}
+module.exports.registerValidation = registerValidation;
