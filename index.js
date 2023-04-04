20f61d7a7bcc108861b68952f27dbe720fd8abfe
Alaa zaza <alaa.kh.zaza@gmail.com>
2023-04-04 21:43:00 +0200

    add mongodb connection, install mongoose and dotenv

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 const express = require('express');
 const app = express(); 
+const dotenv = require('dotenv');
+const mongoose= require('mongoose');
 
-// Impport Routes
+dotenv.config();
+
+// Connect to MongoDB Atlas with Mongoose
+mongoose.connect( process.env.DB_CONNECT, { 
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+mongoose.Promise = global.Promise;
+
+
+// Imoport Routes
 const authRoute = require('./routes/auth');
 
 // Route Middlewares
