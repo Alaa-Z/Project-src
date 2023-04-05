5048e4b163a62cba72f05040506f14f2cd35aa9a
Alaa zaza <alaa.kh.zaza@gmail.com>
2023-04-05 23:42:33 +0200

    change in register route response

--- routes/auth.js
+++ routes/auth.js
@@ -32,7 +32,9 @@ router.post('/register', async (req, res) => {
     });
     try {
         const savedUser = await user.save();
-        res.send({ user: user._id })
+        // res.send({ user: user._id })
+        res.json({ message: 'User registered successfully.' });
+
     } catch (error) {
         res.status(400).send(error)
     }
