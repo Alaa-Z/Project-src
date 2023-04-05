d860236fb35ef65313a131ac624a7642aa877657
Alaa zaza <alaa.kh.zaza@gmail.com>
2023-04-05 10:50:13 +0200

    add login route and validation

--- routes/auth.js
+++ routes/auth.js
@@ -3,7 +3,7 @@ const User = require('../model/User')
 const bcrypt= require('bcryptjs')
 
 // import validate
-const {registerValidation} = require('../validation')
+const {registerValidation, loginValidation} = require('../validation')
 
 
 router.post('/register', async (req, res) => {
@@ -32,9 +32,32 @@ router.post('/register', async (req, res) => {
     });
     try {
         const savedUser = await user.save();
+        res.send({ user: user._id })
     } catch (error) {
         res.status(400).send(error)
     }
 })
 
+// LOGIN
+router.post('/login', async (req, res) => {
+
+    // Validate the data 
+    const {error} = loginValidation(req.body)
+    //   If there is an error 
+    if(error) {
+        return res.status(400).send(error.details[0].message)
+    }
+    // Check if the user exist 
+    const user = await User.findOne({ email : req.body.email})
+    if(!user){
+        return res.status(400).send("Email Not Found ")
+    }
+
+    // Check if the password is correct 
+    const validPassword = await bcrypt.compare(req.body.password, user.password);
+    if(!validPassword){
+        return res.status(400).send("Invalid password")
+    }
+    res.send("Logged in")
+})
 module.exports = router;
--- validation.js
+++ validation.js
@@ -13,4 +13,15 @@ const registerValidation =  (data) => {
     );
     return schema.validate(data)
 }
+
+const loginValidation =  (data) => {
+    const schema = Joi.object(
+        {
+            email: Joi.string().min(6).required().max(255).email(),
+            password: Joi.string().min(6).max(1024).required() 
+        }
+    );
+    return schema.validate(data)
+}
 module.exports.registerValidation = registerValidation;
+module.exports.loginValidation = loginValidation;
