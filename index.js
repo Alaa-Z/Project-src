28f5531859de2a65f34f1d43d3ad07cca132a95e
Alaa zaza <alaa.kh.zaza@gmail.com>
2023-04-05 14:29:43 +0200

    add cors

--- index.js
+++ index.js
@@ -2,6 +2,17 @@ const express = require('express');
 const app = express(); 
 const dotenv = require('dotenv');
 const mongoose= require('mongoose');
+const cors = require('cors')
+
+// CORS middleware
+app.use(cors());
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
 
 // Imoport Routes
 const authRoute = require('./routes/auth');
@@ -18,10 +29,15 @@ mongoose.Promise = global.Promise;
 
 // Middleware
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Route Middlewares
 app.use('/api/user/', authRoute)
 
-app.listen(3000, () => {
+
+
+
+
+app.listen(5000, () => {
     console.log("The Server up and running!")
 })
