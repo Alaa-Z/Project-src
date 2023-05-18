8439068032b3d0bbb464ac3c47e7f2568e52b9da
Alaa zaza <alaa.kh.zaza@gmail.com>
2023-05-18 20:23:42 +0200

    make search case sensitiv

--- routes/book.js
+++ routes/book.js
@@ -136,8 +136,8 @@ router.get('/search/:key', async (req, res) => {
     const books = await Book.find(
       {
       $or: [
-        { title: { $regex: req.params.key } },
-        { author: { $regex: req.params.key } },
+        { title: { $regex: req.params.key, $options: 'i' } }, // with case sensitiv
+        { author: { $regex: req.params.key, $options: 'i' } },
         { ISBN: { $regex: req.params.key } },
       ],
       }).populate('user', ['name', 'address']);
